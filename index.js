13960818a1e77829dd47fad967aedb8715676b0e
rosiedeng1 <rd25500@gmail.com>
2023-12-10 21:29:34 -0500

    exported shapes modules in js

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const {Square, Triangle, Circle} = require('./lib/shapes');
+
 
 inquirer
-  .prompt([
-    {
-    type: 'input',
-    name: 'text',
-    message: 'What is the text for your logo (up to 3 characters)?',
-    },
-    {
-    type: 'input',
-    name: 'textcolor',
-    message: 'What is the text color for your logo?',
-    },
-    {
-    type: 'list',
-    name: 'shape',
-    message: 'What is the shape of your logo?',
-    choices: ['Circle', 'Triangle', 'Square'],
-    },
-    {
-    type: 'input',
-    name: 'shapecolor',
-    message: 'What is your shapes color?',
-    },
+    .prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'What is the text for your logo (up to 3 characters)?',
+        },
+        {
+            type: 'input',
+            name: 'textcolor',
+            message: 'What is the text color for your logo?',
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'What is the shape of your logo?',
+            choices: ['Circle', 'Triangle', 'Square'],
+        },
+        {
+            type: 'input',
+            name: 'shapecolor',
+            message: 'What is your shapes color?',
+        },
 
-  ])
-  .then((data) => {
-    const filename = `logo.svg`;
+    ])
+    .then((data) => {
+        const fileName = `logo.svg`;
+        if (data.shape == 'Triangle') {
+            let triangleShape = new Triangle(data.shapecolor, data.textcolor, data.text)
+            fs.writeFile(fileName, triangleShape.render(), (err) =>
+                err ? console.log(err) : console.log(`Generated logo.svg`)
+            );
+        }
 
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log(`Generated logo.svg`)
-    );
-  });
+        // fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
+        //     err ? console.log(err) : console.log(`Generated logo.svg`)
+        // );
+    });
 
 
 
