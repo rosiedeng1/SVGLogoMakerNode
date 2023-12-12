5c10cecc56a676dcc6cfbee4a82d82e57e6c08b4
rosiedeng1 <rd25500@gmail.com>
2023-12-11 23:41:48 -0500

    added shapestest in js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {Square, Triangle, Circle} = require('./lib/shapes');
+const {Triangle, Circle, Square} = require('./lib/shapes');
 
 
 inquirer
