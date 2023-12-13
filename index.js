e5347f23e9b7abce5009c858a4e32ac1fa6c5fb0
rosiedeng1 <rd25500@gmail.com>
2023-12-13 13:29:38 -0500

    created tests for shapes in js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {Triangle, Circle, Square} = require('./lib/shapes');
+const { Triangle, Circle, Square } = require('./lib/shapes');
 
 
 inquirer
@@ -29,22 +29,41 @@ inquirer
 
     ])
     .then((data) => {
+        // Sets error if logo is more than 3 characters
+        if (data.text.length>3) {
+        throw new Error('Error: Logo must be up to 3 characters')
+        }
+
         const fileName = `logo.svg`;
         if (data.shape == 'Triangle') {
             let triangleShape = new Triangle(data.shapecolor, data.textcolor, data.text)
-            fs.writeFile(fileName, triangleShape.render(), (err) =>
-                err ? console.log(err) : console.log(`Generated logo.svg`)
+            fs.writeFile(fileName,
+
+                // triangleShape.render()
+                triangleShape.insert1stLine() + '\n' +
+                triangleShape.render() + '\n' +
+                triangleShape.insertTextLine() + '\n' +
+                triangleShape.insertLastLine()
+                , (err) =>
+                    err ? console.log(err) : console.log(`Generated logo.svg`)
             );
         }
         if (data.shape == 'Circle') {
             let circleShape = new Circle(data.shapecolor, data.textcolor, data.text)
-            fs.writeFile(fileName, circleShape.render(), (err) =>
+            fs.writeFile(fileName, circleShape.insert1stLine() + '\n' +
+                circleShape.render() + '\n' +
+                circleShape.insertTextLine() + '\n' +
+                circleShape.insertLastLine(), (err) =>
                 err ? console.log(err) : console.log(`Generated logo.svg`)
             );
         }
         if (data.shape == 'Square') {
             let squareShape = new Square(data.shapecolor, data.textcolor, data.text)
-            fs.writeFile(fileName, squareShape.render(), (err) =>
+            fs.writeFile(fileName,
+                squareShape.insert1stLine() + '\n' +
+                squareShape.render() + '\n' +
+                squareShape.insertTextLine() + '\n' +
+                squareShape.insertLastLine(), (err) =>
                 err ? console.log(err) : console.log(`Generated logo.svg`)
             );
         }
